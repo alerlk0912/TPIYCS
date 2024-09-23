9592249bc07a39df506c156f6a4e9d184f06ec68
joemeolans <joemeolans2002@gmail.com>
2024-09-23 19:04:05 -0300

    Agregar atributos en Cliente.java

--- ms-pedidos/src/main/java/isi/dan/ms/pedidos/modelo/Cliente.java
+++ ms-pedidos/src/main/java/isi/dan/ms/pedidos/modelo/Cliente.java
@@ -8,6 +8,9 @@ public class Cliente {
     
     private Integer id;
     private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
     private String correoElectronico;
     private String cuit;
     private String direccion;
